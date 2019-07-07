0f4fd7794bc8f9ed477487fce23d525f2971a83f
Ahsanulhoq <44418887+Ahsanulhoq@users.noreply.github.com>
2019-07-07 21:13:26 +0600

    Add files via upload

---
+++ Tree  Postorder Traversal.cpp
@@ -0,0 +1,33 @@
+
+
+/* you only have to complete the function given below.
+Node is defined as
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+    void postOrder(Node *root) {
+
+      if (root != NULL) {
+        if (root->left)
+          postOrder(root->left);
+        if (root->right)
+          postOrder(root->right);
+        cout << root->data << " ";
+      } else
+        return;
+    }
+
+
